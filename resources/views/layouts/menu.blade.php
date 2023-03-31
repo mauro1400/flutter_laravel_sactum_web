074c58fd511a2e022631423ef946d30d0605f3b6
mauro silva <mauro1400@outlook.es>
2023-03-31 04:07:39 -0400

    Roles

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,11 @@
+<li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
+    <a class="nav-link" href="/home ">
+        <i class=" fas fa-building"></i><span>Dashboard</span>
+    </a>
+    <a class="nav-link" href="/usuarios">
+        <i class=" fas fa-users"></i><span>Usuarios</span>
+    </a>
+    <a class="nav-link" href="/roles">
+        <i class=" fas fa-user-lock"></i><span>Roles</span>
+    </a>
+</li>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\UsuarioController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +19,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::group(['middleware'=>['auth']],function(){
+    Route::resource('roles',RolController::class);
+    Route::resource('usuarios',UsuarioController::class);
 });
